8e8e0efb060dea46aa69825a842b69d20d5ad56e
Romain Moret <romain.moret@epita.fr>
2026-04-23 12:23:53 +1200

    First draggable blocks working

---
+++ Code and Cultivate/Assets/Scripts/UI/TrashZone.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TrashZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null) return;
+
+        DraggableBlock d = eventData.pointerDrag.GetComponent<DraggableBlock>();
+        if (d != null)
+        {
+            d.isToBeDeleted = true;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null) return;
+
+        DraggableBlock d = eventData.pointerDrag.GetComponent<DraggableBlock>();
+        if (d != null)
+        {
+            d.isToBeDeleted = false;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData) {}
+}

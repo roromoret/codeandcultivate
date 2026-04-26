1b43893789843ab57dc6ba5e22fd46a74b045fcd
Romain Moret <romain.moret@epita.fr>
2026-04-26 15:57:46 +1200

    Sequential Activation of the blocks

---
+++ Code and Cultivate/Assets/Scripts/UI/ColumnExecutor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColumnExecutor : MonoBehaviour
+{
+    public Transform blocksContent; 
+
+    public void StartExecution()
+    {
+        StopAllCoroutines(); 
+        
+        ResetAllBlocks();
+
+        StartCoroutine(ExecuteBlocksSequence());
+    }
+
+    private void ResetAllBlocks()
+    {
+        for (int i = 0; i < blocksContent.childCount; i++)
+        {
+            ExecutableBlock block = blocksContent.GetChild(i).GetComponent<ExecutableBlock>();
+            if (block != null)
+            {
+                block.ResetBlock();
+            }
+        }
+    }
+
+    private IEnumerator ExecuteBlocksSequence()
+    {
+        for (int i = 0; i < blocksContent.childCount; i++)
+        {
+            Transform child = blocksContent.GetChild(i);
+            ExecutableBlock block = child.GetComponent<ExecutableBlock>();
+            
+            if (block != null)
+            {
+                yield return StartCoroutine(block.Execute());
+            }
+        }
+    }
+}

f15b42703accefa7231f1ba7a0f851e6f433c190
Romain Moret <romain.moret@epita.fr>
2026-06-01 19:28:36 +1200

    design and animations

---
+++ Code and Cultivate/Assets/Scripts/Camera/DepthSorter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+// In order to show the player right with all the crops
+public class DepthSorter : MonoBehaviour
+{
+    private SortingGroup sortingGroup;
+    private SpriteRenderer spriteRenderer;
+    
+    [SerializeField] private float yOffset = 0f;
+
+    void Start()
+    {
+        sortingGroup = GetComponent<SortingGroup>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        int order = Mathf.RoundToInt(-(transform.position.z + yOffset) * 100f);
+
+        if (sortingGroup != null)
+        {
+            sortingGroup.sortingOrder = order;
+        }
+        else if (spriteRenderer != null)
+        {
+            spriteRenderer.sortingOrder = order;
+        }
+    }
+}
---
+++ Code and Cultivate/Assets/Scripts/Camera/TextSortingLayerFix.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class TextSortingLayerFix : MonoBehaviour
+{
+    public int superPriorityOrder = 10000; 
+
+    void Start()
+    {
+        TextMeshPro textComponent = GetComponent<TextMeshPro>();
+        
+        if (textComponent != null)
+        {
+            textComponent.sortingOrder = superPriorityOrder;
+        }
+        else
+        {
+            Debug.LogWarning($"No TextMeshPro found on {gameObject.name}.");
+        }
+    }
+}
